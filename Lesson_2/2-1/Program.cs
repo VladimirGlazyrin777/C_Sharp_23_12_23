f404f9a7a79004bc9746a1efc473b59d847dc81e
Vladimir_Glazyrin <crv59@yandex.ru>
2023-12-26 13:28:00 +0500

    add Lesson_2

---
+++ Lesson_2/2-1/Program.cs
@@ -0,0 +1,5 @@
+﻿// напишите программу, которая принимает на вход трехзначное
+//число и удаляет вторую цифру этого числа.
+Console.WriteLine("Enter Number: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a / 100 * 10 + a % 10);
---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимаетна вход
+// трехзначное число и возводить вторую цифру этого числа в степень, равную
+//третьей цифре.
+
+int a = int.Parse(Console.ReadLine()!);
+int SecondNumb = a / 10 % 10;
+int ThirdNumb = a % 10;
+Console.WriteLine(Math.Pow(SecondNumb, ThirdNumb));
+
