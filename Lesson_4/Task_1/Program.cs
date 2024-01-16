0b773f335513a7379c0d53c704253b239a410817
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-16 21:00:05 +0500

    Добавил семинар_4

---
+++ Lesson_4/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте одномерный массив, заполненный случайными числами.
+//Определите количество простых чисел в этом массиве.
+class Programm
+{
+static void Main()
+{
+Random random = new Random();
+int[] numbers = new int[10];//Создаем массив из 10 случайных чисел.
+for(int i = 0; i < numbers.Length; i++)
+  {
+   numbers[i]=random.Next(1, 100);
+  }
+  int count = 0;
+  foreach(var number in numbers)
+  {
+    if(IsPrime(number))
+    {
+        Console.WriteLine(number);
+        count++;
+    }
+  }
+Console.WriteLine("Количество простых чисел в массиве:" +count);
+}
+//Метод для определения является ли число простым
+static bool IsPrime(int number)
+{
+if(number <2) return false;
+for (int i = 2;i <number; i++)
+{
+    if(number % i == 0)
+    {
+        return false;
+    }
+  }
+  return true;
+ }
+}
---
+++ Lesson_4/Task_3/Program.cs
@@ -0,0 +1,40 @@
+﻿//Заполнить массив на N (вводится с консоли, не более 8)
+//случайных целых чисел от 0 до 9.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+long Int1(int[] arr)
+{
+    string num = "";
+    foreach (int item in arr)
+    {
+        num += item;
+    }
+    return long.Parse(num);
+}
+long result = Int1(mass);
+Console.WriteLine(result);
