08bfc6e83803ac4513b24cf4dd17da332447d571
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-23 15:48:36 +0500

    Лекция 2мерные массивы задача_2,3

--- Lecture/Двумерные массивы/Task_2/Program.cs
+++ Lecture/Двумерные массивы/Task_2/Program.cs
@@ -1,7 +1,5 @@
 ﻿//Код не работает!
 
-
-
 int[,] CreateMatrix(int rowCount, columsCount)
 {
     int[,] matrix = new int[rowCount, columsCount];
@@ -11,12 +9,42 @@ int[,] CreateMatrix(int rowCount, columsCount)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(1, 11);
+            matrix[i, j] = rnd.Next(1, 1000);
         }
     }
     return matrix;
 }
 
+int[,] matrix CreateMatrix(3,4);
+foreach (int e in matrix)
+{
+    if (isInteresting(e) == true)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+bool isInteresting(int value)
+{
+    int sumOfDigist = GetSumOfDigits(value);
+    if (sumOfDigist % 2 == 0)
+    {
+        return true;
+    }
+        return false;
+}
+
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while(value > 0)
+    {
+        sum +=value % 10;
+        value /=10;
+    }
+    return sum;
+}
 void ShowMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -28,8 +56,3 @@ void ShowMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
-
-int[,] matrix = CreateMatrix(4, 5);
-ShowMatrix(matrix);
-
-
---
+++ Lecture/Двумерные массивы/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Код не работает!
+
+
+string GetLengthFromStr(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters +e;
+        }
+    }
+    return letters;
+}
+
+
+string str = Console.ReadLine();
+string letters = GetLengthFromStr(str);
+Console.WriteLine(letters);
