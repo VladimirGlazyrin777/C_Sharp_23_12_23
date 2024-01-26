c7688964943a3879e48217e584bec1fb2ea4e728
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-26 14:32:36 +0500

    Лекция рекурсия задача 1

---
+++ Lecture/Рекурсия/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿void OpenMatryoshka(int size)
+{
+    if (size == 1)
+    {
+        Console.WriteLine("Smallest!");
+        return;
+    }
+     OpenMatryoshka(size - 1);
+    Console.WriteLine($"Opening matryoshka of size {size}...");
+}
+ OpenMatryoshka(5);
