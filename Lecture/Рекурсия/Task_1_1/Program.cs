573a20611bae97b3cce4af99d5ef4c72a64cf504
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-26 14:42:08 +0500

    Лекция рекурсия задача 1_1

---
+++ Lecture/Рекурсия/Task_1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Вычислить факториал от натурального числа.
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * Fact(n - 1);
+}
+
+Console.Write(Fact(5));
