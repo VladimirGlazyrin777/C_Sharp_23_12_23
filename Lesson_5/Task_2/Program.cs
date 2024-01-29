0e911ab1579d70ec7372015a74a7fa58034939ba
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-29 16:39:36 +0500

    Lesson_5\Task_2

--- Lesson_5/Task_2/Program.cs
+++ Lesson_5/Task_2/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿// 2. Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+int[,] CreateMatrix(int rowCount, int columsCount)//Функция создания матрицы
+{
+    int[,] matrix = new int[rowCount, columsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)//функция вывода матрицы на экран.
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }  
+        Console.WriteLine();
+    }
+}
+int[,] matrix = CreateMatrix(3,4);//Задание размера матрицы и инициализация "интересного" числа.
+ShowMatrix(matrix); 
+  
+int sumDiagonal(int[,] array)
+ {
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+       for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i==j){sum=array[i,j]+sum;}
+        }  
+    } 
+    return sum;
+ } 
+
+Console.WriteLine(sumDiagonal(matrix));
